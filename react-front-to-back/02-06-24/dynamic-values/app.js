53f4e6c1cf93d2942e9709e09021f4f3a0c6c779
Alokananda Y <plantsfortrees@gmail.com>
2024-06-02 18:57:45 +0530

    Blog post

--- react-front-to-back/02-06-24/dynamic-values/app.js
+++ react-front-to-back/02-06-24/dynamic-values/app.js
@@ -1,15 +1,28 @@
 const app = document.getElementById("app");
 
 function App() {
-	const title = "Blog title";
-	const body = "This is a blog post";
+  const title = "Blog title";
+  const body = "This is a blog post";
+  const comments = [
+    { id: 1, text: "Comment one" },
+    { id: 2, text: "Comment two" },
+    { id: 3, text: "Comment three" },
+  ];
 
-	return (
-		<div>
-			<h1>{title}</h1>
-			<p>{body}</p>
-		</div>
-	)
+  return (
+    <div>
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+      <div>
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(app);

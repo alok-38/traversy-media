e4147b9c79e56063c43783524231c7c1284dcfd4
Alokananda Y <plantsfortrees@gmail.com>
2024-06-26 12:16:52 +0530

    prisma and neon set up

---
+++ expense-tracker/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Expense Tracker",
+  description: "Track your expenses and create a budget",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={roboto.className}>
+          <main className="container">{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
